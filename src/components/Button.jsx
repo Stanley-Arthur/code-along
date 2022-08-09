ee1997111b6be67c3c6834fe181de37f765c89cc
Stanley Essuman Arthur <essumanarthur24@gmail.com>
2022-08-09 02:05:24 -1200

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export const Button1 = () => {
+    return <button>Add to cart</button>
+};
+export const Button2 = () => {
+    return <button>Login</button>
+};
+export const Button3 = () => {
+    return <button>Register</button>
+};
+
---
+++ src/components/image.jsx
@@ -0,0 +1,6 @@
+import robot from "../asset/image/robot.jpg";
+
+
+export const Image = () => {
+    return <img src={robot} alt="" />
+}
